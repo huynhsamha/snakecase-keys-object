a629467bbb274362f9d62cdedc20ccd7ba139fb1
huynhsamha <huynhsamha@gmail.com>
2018-03-06 21:42:30 +0700

    update readme

--- test/index.js
+++ test/index.js
@@ -17,19 +17,19 @@ const object = {
 const res = snakeKeys(object);
 
 console.log(res);
-/** {
-	camel_case: 1,
-	uppercase: 2,
-	lowercase: 3,
-	snake_case: 4,
-	pascal_case: 5,
-	title_case: 6,
-	dot_case: 7,
-	param_case: 8,
-	sentence_case: 9,
-	path_case: 10,
-	header_case: 11
-} */
+/**
+ { camel_case: 1,
+   uppercase: 2,
+   lowercase: 3,
+   snake_case: 4,
+   pascal_case: 5,
+   title_case: 6,
+   dot_case: 7,
+   param_case: 8,
+   sentence_case: 9,
+   path_case: 10,
+   header_case: 11 }
+ */
 
 console.log(snakeKeys(null)); // null
 console.log(snakeKeys(undefined)); // null
